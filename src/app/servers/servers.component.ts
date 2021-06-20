5af9f20a729cabc51bda6a846ddb28555ccb4817
raxsi <shenoyrakshith96@gmail.com>
2021-06-20 18:21:46 +0530

    Two way binding comment

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
 ToggleButtonserver=false;
 ServercreateStatus='Server is not been created';
+ ServerNameUpdate='Test Server';
   constructor() { 
     setTimeout(()=>{this.ToggleButtonserver=true;},2000);
   }
@@ -19,5 +20,10 @@ ServercreateStatus='Server is not been created';
   onServerclick()
   {
     this.ServercreateStatus='Server is created on click';
+  }
+
+  OnUpdateName(event:any)
+  {
+  this.ServerNameUpdate = (<HTMLInputElement>event.target).value; // Cast event.target to the appropriate HTML element to ensure it is
   }
 }
