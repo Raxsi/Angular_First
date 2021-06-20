986b3228432d2776d4494775308c4c3d5878fdf9
raxsi <shenoyrakshith96@gmail.com>
2021-06-20 13:27:25 +0530

    string interpolation changes

--- src/app/server/Server.component.ts
+++ src/app/server/Server.component.ts
@@ -3,12 +3,22 @@ import { Component } from '@angular/core';
 
 
 @Component({
-    selector: 'app-server',
-    //templateUrl:'./server.component.html'
-    template: `<app-server></app-server>
+   // selector: 'app-server',
+  // selector: '[app-server]', //div component
+   selector:'.app-server', //selectorapp-server is class
+    templateUrl:'./server.component.html'
+    /*template: `<app-server></app-server>
     <p> hello within app-server</p>
-    <app-server></app-server>`
+    <app-server></app-server>` */
+    
 })
 export class ServerComponent{
+ ServerID: number =10;
+ status: string='online';
 
+
+ getstatus()
+ {
+     return this.status;
+ }
 }
