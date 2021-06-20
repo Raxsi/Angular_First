4a0e00221c114c8530a8357bf19294fcea7f0600
raxsi <shenoyrakshith96@gmail.com>
2021-06-20 19:53:37 +0530

    ng changes commit

--- src/app/server/Server.component.ts
+++ src/app/server/Server.component.ts
@@ -6,16 +6,28 @@ import { Component } from '@angular/core';
    // selector: 'app-server',
   // selector: '[app-server]', //div component
    selector:'.app-server', //selectorapp-server is class
-    templateUrl:'./server.component.html'
+    templateUrl:'./server.component.html',
     /*template: `<app-server></app-server>
     <p> hello within app-server</p>
     <app-server></app-server>` */
-    
+    styles:[`
+    .online{
+        color:yellow
+    }`]
 })
 export class ServerComponent{
  ServerID: number =10;
- status: string='online';
+ status: string='offline';
 
+constructor()
+{
+    this.status = Math.random() < 0.5 ? 'online':'offline';
+}
+
+getcolor()
+{
+    return this.status==='online'?'green':'red';
+}
 
  getstatus()
  {
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,6 +9,8 @@ export class ServersComponent implements OnInit {
 ToggleButtonserver=false;
 ServercreateStatus='Server is not been created';
  ServerNameUpdate='Test Server';
+ ServerUpdated=false;
+ servers=['Test Server','Development Server'];
   constructor() { 
     setTimeout(()=>{this.ToggleButtonserver=true;},2000);
   }
@@ -19,10 +21,12 @@ ServercreateStatus='Server is not been created';
 
   onServerclick()
   {
-    this.ServercreateStatus='Server is created on click';
+    this.ServerUpdated=true;
+    this.servers.push(this.ServerNameUpdate);
+    this.ServercreateStatus='Server is created on click '+ 'Server name is'+' ' +this.ServerNameUpdate;
   }
 
-  OnUpdateName(event:any)
+  OnUpdateName(event:Event)
   {
   this.ServerNameUpdate = (<HTMLInputElement>event.target).value; // Cast event.target to the appropriate HTML element to ensure it is
   }
